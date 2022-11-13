bca2e9ab2465d4f7aca6b3cdbfee922da86224f9
Khaled Ahmed <77308030+khhaledahmaad@users.noreply.github.com>
2022-11-13 23:20:39 +0000

    updated python scripts

--- python scripts/01a_retrieve_sms_from_twilio.py
+++ python scripts/01a_retrieve_sms_from_twilio.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### Scope
+# This notebook shows how to retrieve sms's from Twilio for a specific time duration
+
+# In[1]:
+
+
 # Import libraries
 import os
 import pathlib
@@ -6,7 +15,12 @@ import pandas as pd
 from datetime import datetime
 from twilio.rest import Client
 
+
+# In[2]:
+
+
 # Set paths
+
 # Path to all data directories
 data = pathlib.Path(r'C:\Users\ahmedk40\Documents\GitHub\Attendance-Monitoring-via-SMS\data')
 
@@ -19,7 +33,15 @@ processed = data/'processed'
 # Path to environment variables
 env_vars = pathlib.Path(r'C:\Users\ahmedk40\Documents\GitHub\Attendance-Monitoring-via-SMS\env vars')
 
+
+# In[3]:
+
+
 # Load environment variables from .env.txt file
+
+"""
+The following section reads a env file to load twilio credentials saved in a '.env' file
+"""
 load_dotenv(env_vars/".env.txt")
 
 # Create twilio client object
@@ -27,11 +49,19 @@ account_sid = os.environ.get("TWILIO_ACCOUNT_SID")
 auth_token = os.environ.get("TWILIO_AUTH_TOKEN")
 client = Client(account_sid, auth_token)
 
+
+# In[4]:
+
+
 # Retrieve sms using the client object
 messages = client.messages.list(
     date_sent_after=datetime(2022, 11, 5, 12, 30, 0),
     date_sent_before=datetime(2022, 11, 5, 12, 40, 0)
 )
+
+
+# In[5]:
+
 
 # Append the retrieved sms's to a dataframe
 d = []
@@ -40,8 +70,17 @@ for message in messages:
 
 df = pd.DataFrame(d ,columns=['from' ,'message' ,'status' ,'date_ent'])
 
+
+# In[6]:
+
+
 # Preview
 print(df.head())
 
+
+# In[7]:
+
+
 # Export the dataframe to a CSV file
 df.to_csv(raw/'smsLogs.csv', index=False, encoding='utf-8')
+
