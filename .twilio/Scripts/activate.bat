463b0f738b2521c36de855e7c7b9a1d256374043
Khaled Ahmed <77308030+khhaledahmaad@users.noreply.github.com>
2023-02-05 15:00:26 +0000

    updated .gitignore

--- .twilio/Scripts/activate.bat
+++ .twilio/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\ahmedk40\Documents\attendance_monitoring_via_sms\.twilio
+set VIRTUAL_ENV=C:\Users\ahmedk40\Documents\GitHub\attendance_monitoring_via_sms\.twilio
 
 if not defined PROMPT set PROMPT=$P$G
 
